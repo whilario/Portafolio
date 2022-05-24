074f699e6db2f1f6e887dca7e3da1cc66b30ac75
Wilson Hilario <wilsonr_025@hotmail.com>
2022-05-24 07:38:26 -0400

    agregando configuracion para hots

---
+++ server.js
@@ -0,0 +1,12 @@
+express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/index.html");
+})
+app.listen(process.env.PORT || 3000, function()
+{
+  console.log("Server is running on port 3000");
+});
